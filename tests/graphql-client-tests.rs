ac5ea79b311b924271500d6d9462d2bf48583475
Emile Fugulin <code@efugulin.com>
2024-10-25 22:17:55 -0400

    Bug reproduction

--- tests/graphql-client-tests.rs
+++ tests/graphql-client-tests.rs
@@ -1,6 +1,7 @@
 use std::{future::IntoFuture, time::Duration};
 
 use assert_matches::assert_matches;
+use async_tungstenite::tungstenite::{client::IntoClientRequest, http::HeaderValue};
 use futures_lite::{future, StreamExt};
 use graphql_client::GraphQLQuery;
 use graphql_ws_client::graphql::StreamingOperation;
@@ -19,8 +20,6 @@ struct BooksChanged;
 
 #[tokio::test]
 async fn main_test() {
-    use async_tungstenite::tungstenite::{client::IntoClientRequest, http::HeaderValue};
-
     let server = SubscriptionServer::start().await;
 
     sleep(Duration::from_millis(20)).await;
@@ -41,7 +40,7 @@ async fn main_test() {
 
     tokio::spawn(actor.into_future());
 
-    let stream = client.subscribe(build_query()).await.unwrap();
+    let mut stream = client.subscribe(build_query()).await.unwrap();
 
     sleep(Duration::from_millis(100)).await;
 
@@ -60,24 +59,79 @@ async fn main_test() {
         },
     ];
 
-    future::zip(
-        async {
-            for update in &updates {
-                server.send(update.to_owned()).unwrap();
-            }
-        },
-        async {
-            let received_updates = stream.take(updates.len()).collect::<Vec<_>>().await;
+    inner(&server, &updates).await;
 
-            for (expected, update) in updates.iter().zip(received_updates) {
-                let update = update.unwrap();
-                assert_matches!(update.errors, None);
-                let data = update.data.unwrap();
-                assert_eq!(data.books.id, expected.id.0);
-            }
-        },
-    )
-    .await;
+    server.send(updates[0].to_owned()).unwrap();
+
+    let update = tokio::time::timeout(Duration::from_millis(1000), stream.next())
+        .await
+        .unwrap()
+        .unwrap()
+        .unwrap();
+    let data = update.data.unwrap();
+    assert_eq!(data.books.id, updates[0].id.0);
+
+    let update2 = tokio::time::timeout(Duration::from_millis(1000), stream.next())
+        .await
+        .unwrap()
+        .unwrap()
+        .unwrap();
+    let data = update2.data.unwrap();
+    assert_eq!(data.books.id, updates[0].id.0);
+
+    let result = tokio::time::timeout(Duration::from_millis(100), stream.next()).await;
+    assert!(result.is_err());
+
+    // future::zip(
+    //     async {
+    //         for update in &updates {
+    //             server.send(update.to_owned()).unwrap();
+    //         }
+    //     },
+    //     async {
+    //         let received_updates = stream.take(updates.len()).collect::<Vec<_>>().await;
+
+    //         for (expected, update) in updates.iter().zip(received_updates) {
+    //             let update = update.unwrap();
+    //             assert_matches!(update.errors, None);
+    //             let data = update.data.unwrap();
+    //             assert_eq!(data.books.id, expected.id.0);
+    //         }
+    //     },
+    // )
+    // .await;
+}
+
+async fn inner(server: &SubscriptionServer, updates: &[subscription_server::BookChanged]) {
+    let mut request = server.websocket_url().into_client_request().unwrap();
+    request.headers_mut().insert(
+        "Sec-WebSocket-Protocol",
+        HeaderValue::from_str("graphql-transport-ws").unwrap(),
+    );
+
+    let (connection, _) = async_tungstenite::tokio::connect_async(request)
+        .await
+        .unwrap();
+
+    println!("Connected");
+
+    let (client, actor) = graphql_ws_client::Client::build(connection).await.unwrap();
+
+    tokio::spawn(actor.into_future());
+
+    let mut stream2 = client.subscribe(build_query()).await.unwrap();
+
+    sleep(Duration::from_millis(100)).await;
+
+    server.send(updates[0].to_owned()).unwrap();
+
+    let update = tokio::time::timeout(Duration::from_millis(1000), stream2.next())
+        .await
+        .unwrap()
+        .unwrap()
+        .unwrap();
+    let data = update.data.unwrap();
+    assert_eq!(data.books.id, updates[0].id.0);
 }
 
 #[tokio::test]
